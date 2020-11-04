18159446b966687988cdb976dac62c99360d21d0
Rufin Hounkpe <rhounkpe@gmail.com>
2020-11-04 09:59:15 +0100

    Test coverage analysis: add jacoco plugin

---
+++ src/main/java/be/digitcom/fruitsvegatables/FruitsVegatablesSbApiApplication.java
@@ -0,0 +1,13 @@
+package be.digitcom.fruitsvegatables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitsVegatablesSbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitsVegatablesSbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/digitcom/fruitsvegatables/FruitsVegatablesSbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.digitcom.fruitsvegatables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitsVegatablesSbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
